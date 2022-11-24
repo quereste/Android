bf79da6a8039a4203a192eac5a7c7ce04b537143
monter <tmszczepanik@outlook.com>
2022-11-24 18:41:19 +0100

    Add swipe

--- Task_4/ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ Task_4/ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.ActivityMainBinding
@@ -24,6 +25,36 @@ class MainActivity : AppCompatActivity() {
         taskList.adapter = adapter
         taskList.layoutManager = LinearLayoutManager(this)
 
+       ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT)  {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false;
+            }
 
+           override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+               tasks.removeAt(viewHolder.adapterPosition)
+
+               adapter.notifyItemRemoved(viewHolder.adapterPosition)
+           }
+            }).attachToRecyclerView(taskList)
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT)  {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false;
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                tasks.removeAt(viewHolder.adapterPosition)
+
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+        }).attachToRecyclerView(taskList)
     }
 }
